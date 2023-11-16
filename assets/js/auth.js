90353c46555b685e50a2ae7f3c13f64a43487b4b
ASAP64 <asap.asso64@gmail.com>
2023-11-16 08:21:11 +0100

    beta

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,46 @@
-// JavaScript Document
+// auth.js
+const authStatus = document.getElementById('authStatus');
+const loginForm = document.getElementById('loginForm');
+const registerForm = document.getElementById('registerForm');
+
+// Soumettre le formulaire de connexion
+loginForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            const user = userCredential.user;
+            authStatus.innerHTML = `Connecté en tant que ${user.email}`;
+            
+            // Redirection vers la page d'ajout d'informations après la connexion
+            window.location.href = "ajouter-informations.html";
+        })
+        .catch(error => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            authStatus.innerHTML = `Erreur de connexion: ${errorMessage}`;
+        });
+});
+
+// Soumettre le formulaire d'enregistrement
+registerForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const email = document.getElementById('registerEmail').value;
+    const password = document.getElementById('registerPassword').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            const user = userCredential.user;
+            authStatus.innerHTML = `Utilisateur enregistré en tant que ${user.email}`;
+            
+            // Redirection vers la page d'ajout d'informations après l'enregistrement
+            window.location.href = "ajouter-informations.html";
+        })
+        .catch(error => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            authStatus.innerHTML = `Erreur d'enregistrement: ${errorMessage}`;
+        });
+});
--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,1 +1,26 @@
-// JavaScript Document
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD8HuicHwscY4O4wFMbuXZZo13p-OOSVjI",
+    authDomain: "asapsite-e622c.firebaseapp.com",
+    projectId: "asapsite-e622c",
+    storageBucket: "asapsite-e622c.appspot.com",
+    messagingSenderId: "526561426439",
+    appId: "1:526561426439:web:8bbd41ed5a9a24fafebea2",
+    measurementId: "G-KD0XFF487W"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  // Initialiser Firestore
+  const firestore = firebase.firestore();
+</script>
